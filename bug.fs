ce5e4d588eee3cfba8b8fbfc1930a3f890239cba
BugHunter-X <askzenai@gmail.com>
2025-02-14 06:50:04 +0100

    Add bug.fs file

---
+++ bug.fs
@@ -0,0 +1,15 @@
+let mutable x = 0
+let mutable y = 0
+
+let rec loop () =
+    if x < 10 then
+        x <- x + 1
+    else
+        y <- y + 1
+
+    if y < 5 then
+        loop ()
+    else
+        printfn "%d %d" x y
+
+loop ()
